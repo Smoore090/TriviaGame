b6ee5b138d85d9eb582ec05fc21d8477e551426f
Spencer Moore <44985476+Smoore090@users.noreply.github.com>
2019-01-13 19:24:54 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+    $(".questions").css("display","none");
+    $(".results").css("display", "none");
+    
+    
+    //start the game
+    $("#start").on("click", start); 
+    
+    //submit answers if done before timer goes off
+    //$("#submit").on("click", submit); 
+    
+    var correct = 0;
+    var incorrect = 0;
+    
+    $("input").on("click",function(){
+      if (this.className === "correct"){
+       ++ correct; 
+       $("#numberCorrect").text(correct);
+       console.log(correct);
+      }
+      else {
+      console.log(this.className);
+       ++ incorrect;
+       $("#numberIncorrect").text(incorrect);
+       console.log(incorrect);
+    }
+    });
+    //make values display on browser within the HTML:
+    
+    
+    
+    
+    
+    
+    // function for start button, starts the countdown timer:
+    function start() {
+    var myVar = setInterval(myTimer, 1000);
+    
+    var countDown = 30
+    
+    $(".questions").css("display", "block");
+    
+      function myTimer() {
+        countDown --
+        document.getElementById("timeRemaining").innerHTML = countDown;
+        if (countDown === 0) {
+          console.log("timer reached zero");
+          $(".questions").css("display","none");
+          $(".results").css("display", "block");
+          clearInterval(myVar);    //the name of my interval is 'myVar'
+        };
+      };
+    }; 
+    
+    //function for submit button
+       function submit() {
+    
+        
+    
+        $(".questions").css("display","none");
+          $(".results").css("display", "block");
+          
+          console.log("clicked submit button");
+        }
+    
+    
+    });
+    
+    // questions are revealed
+    // submit button is revealed
+    
+    // when time runs out...same function as clicking submit
